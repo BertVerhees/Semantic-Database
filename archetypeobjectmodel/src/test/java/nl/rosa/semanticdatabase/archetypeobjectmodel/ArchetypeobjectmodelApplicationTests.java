c96ad11993ad6db3f0768a75bc52f35a3dde41aa
BertVerheesGeonovum <b.verhees@geonovum.nl>
2020-04-21 16:02:28 +0200

    aom

---
+++ archetypeobjectmodel/src/test/java/nl/rosa/semanticdatabase/archetypeobjectmodel/ArchetypeobjectmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rosa.semanticdatabase.archetypeobjectmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchetypeobjectmodelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
