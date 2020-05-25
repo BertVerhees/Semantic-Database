8a36967751cc41ee99de556c4295292518fcbca7
BertVerheesGeonovum <b.verhees@geonovum.nl>
2020-05-25 09:37:48 +0200

    repositories bmmdata classes

---
+++ bmmdata/src/main/java/nl/rosa/semanticdatabase/bmmdata/repositories/classes/BmmGenericClassRepository.java
@@ -0,0 +1,11 @@
+package nl.rosa.semanticdatabase.bmmdata.repositories.classes;
+
+import nl.rosa.semanticdatabase.bmm.classes.BmmGenericClass;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Default created on 25-5-2020
+ */
+
+public interface BmmGenericClassRepository extends CrudRepository<BmmGenericClass, Long> {
+}
